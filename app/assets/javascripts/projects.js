afb56f20e37dc7c6b72c7a5d73c514f4bc3dc485
jsp <jspenkar@gmail.com>
2014-10-23 14:40:57 -0500

    Color scheme changed.

---
+++ app/assets/javascripts/projects.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	$('#revenuelink').on('click', function(){
+		$('#revenue').toggle()
+	})
+	$('#incomelink').on('click', function(){
+		$('#income').toggle()
+	})
+	$('#balancelink').on('click', function(){
+		$('#balance').toggle()
+	})
+	$('#cashflowlink').on('click', function(){
+		$('#cashflow').toggle()
+	})
+	$('#ratiolink').on('click', function(){
+		$('#ratio').toggle()
+	})
+})
