5992903d7575695d3cfb56861f3821989877fdb1
jsp <jspenkar@gmail.com>
2014-10-23 18:26:35 -0500

    Income From Operations Complete.

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -22,30 +22,61 @@ $(document).ready(function(){
 
 
 })
+
+
 var totalRevenue = 0;
 var proforma = function(){
 	totalRevenue = 0;
-	proFormaRevenue(totalRevenue);
+	proFormaRevenueCost(totalRevenue);
 	proFormaIncomeStmt()
 	console.log(totalRevenue);
 }
 
+////////////////////////
+// begin proforma revenue & cost
+////////////////////////
 
-var proFormaRevenue = function(){
+var proFormaRevenueCost = function(){
 	var source = [], rev = [];
-	$('#revenue tr').each(function(){source.push($(this).attr('class'))})
-	$('#revenue tr').each(function(){rev.push($(this).children().first().html())})
+	var newCostAndExp = 0
+	var priorTotalRevenue = parseInt($('.total_revenue').children(':nth-child(1)').html())
+	$('#revenue tr').each(function(){source.push($(this).attr('class'))});
+	$('#revenue tr').each(function(){rev.push($(this).children().first().html())});
 	for(var i = 0; i < source.length ; i ++){
+		var spot = i;
 		ans = prompt("What are your growth expectations for next year's " + source[i] + "segment?");
-		var newRevenue = parseInt(rev[i]) * parseInt(ans)
-		totalRevenue += newRevenue
-		var spot = i
-		$('#revenue tbody').children(":nth-child(" + (spot + 1) + ")").prepend('<td>' + newRevenue + '</td>')
+		var newRevenue = parseInt(rev[i]) * parseInt(ans);
+		totalRevenue += newRevenue;
+		$('#revenue tbody').children(":nth-child(" + (spot + 1) + ")").prepend('<td>' + newRevenue + '</td>');
 	}
-	$('.total_revenue').prepend('<td>' + totalRevenue + '</td>')
+	$('.total_revenue').prepend('<td>' + totalRevenue + '</td>');
+	var newCost = parseInt($('.cost_and_expenses').children(':nth-child(1)').html()) / totalRevenue;
+	var nextCostAndExpense = parseInt(newCost * priorTotalRevenue)
+	$('.cost_and_expenses').prepend('<td>' + nextCostAndExpense + '</td>');	
+	alert('Depreciation is assumed to remain constant.');
+	var costGrow = parseInt(prompt('Enter R&D, SG&A and Advertising expense expectations compared to most recent year.'));
+	$('.depreciation').prepend('<td>' + parseInt($('.depreciation').children(':nth-child(1)').html()) + '</td>')
+	$('.advertising').prepend('<td>' + costGrow * parseInt($('.advertising').children(':nth-child(1)').html()) + '</td>')
+	$('.research_and_development').prepend('<td>' + costGrow * parseInt($('.research_and_development').children(':nth-child(1)').html()) + '</td>')
+	$('.sales_general_and_administrative').prepend('<td>' + costGrow * parseInt($('.sales_general_and_administrative').children(':nth-child(1)').html()) + '</td>')
+	newCostAndExp += parseInt($('.depreciation').children(':nth-child(1)').html())
+	newCostAndExp += parseInt($('.advertising').children(':nth-child(1)').html())
+	newCostAndExp += parseInt($('.research_and_development').children(':nth-child(1)').html())
+	newCostAndExp += parseInt($('.sales_general_and_administrative').children(':nth-child(1)').html())
+	newCostAndExp += nextCostAndExpense
+	$('.total_costs_and_expenses').prepend('<td>' + newCostAndExp + '</td>')
+	$('.income_from_operations').prepend('<td>' + parseInt(totalRevenue - newCostAndExp) + '</td>')
 }
+
+////////////////////////
+// end proforma revenue
+////////////////////////
+
 
 var proFormaIncomeStmt = function(){
-	var newCost = totalRevenue / parseInt($('.cost_and_expenses').children(':nth-child(1)').html())
-	$('.cost_and_expenses').prepend('<td>' + newCost + '</td>')	
 }
+
+
+
+
+
