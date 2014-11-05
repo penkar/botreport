bc8f16901fed9f0ceee904a330096d229111af2d
jsp <jspenkar@gmail.com>
2014-11-04 21:55:28 -0600

    Javascript Refactors.

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,37 +1,24 @@
 $(document).ready(function(){
 	$('#proforma').on('click', function(){
-		proforma()
+		$('#ratio').hide(1000);
+		proforma();
+		$('#ratiolink').addClass('selected');
 	})
 	$("#commonsize").on("click", function () {
 		////////////////////ADDD FUNCTIONALITY TO CLOSE RATIOS AND HIGHLIGHT RATIOS BUTTON WHEN COMMONSIZE IS HIT. POSSIBLLY ADD ID TO RATIO LINK?
-		$(this).toggleClass('selected')
+		$('#ratiolink').addClass('selected');
+		$('#ratio').hide(1000);
+		$(this).toggleClass('selected');
 	  _.each($('.column'), function(td){
 	  	$(td).text($(td).text() == $(td).data("original") ? $(td).data("cs") : $(td).data("original"));
-	  	$(td).toggle('slow').toggle('slow')
+	  	$(td).toggle('slow').toggle('slow');
 	  })
 	});
 	$('.proj').on('click', function(){
-		$(this).toggleClass('selected')
-		$('#'+$(this).data('link')).toggle()
-	})
-})
-
-var totalRevenue = 0;
-var proforma = function(){
-	totalRevenue = 0;
-	proFormaRevenueCost(totalRevenue);
-	proFormaNetIncome()
-	proFormaIncomeStmt();
-	console.log(totalRevenue);
-}
-
-/////////////////////////////////////////
-//Remove $ and commas, then parse to Int.
-/////////////////////////////////////////
-var setToNum = function(number){
-	number = parseFloat(number.replace('$','').replace(',',''));
-	return number;
-}
+		$(this).toggleClass('selected');
+		$('#'+$(this).data('link')).toggle();
+	});
+});
 
 /////////////////////////////////////////
 //Get first Child of class
@@ -46,67 +33,121 @@ var getRatio = function(classType){
 	return ans;
 }
 
-////////////////////////
-// begin proforma revenue & cost
-////////////////////////
-
-var proFormaRevenueCost = function(){
-	var source = [], rev = [];
-	var newTotalCostandExp = 0;
-	var priorTotalRevenue = firstChildGet('.total_revenue');
-	$('#revenue tr').each(function(){source.push($(this).attr('class'))});
-	$('#revenue tr').each(function(){rev.push($(this).children().first().html())});
-	for(var i = 0; i < source.length ; i ++){
-		var spot = i;
-		ans = prompt("What are your growth expectations for next year's " + source[i] + " segment?");
-		var newRevenue = setToNum(rev[i]) * setToNum(ans);
-		totalRevenue += newRevenue;
-		$('#revenue tbody').children(":nth-child(" + (spot + 1) + ")").prepend('<td>' + newRevenue + '</td>');
-	}
-	$('.total_revenue').prepend('<td>' + totalRevenue + '</td>');
-	var priorCost = firstChildGet('.cost_and_expenses');
-	var newCostandExp = (priorCost / priorTotalRevenue * totalRevenue);
-	$('.cost_and_expenses').prepend('<td>' + newCostandExp.toFixed(2) + '</td>');	
-	alert('Depreciation is assumed to remain constant.');
-	var costGrow = setToNum(prompt('Enter R&D, SG&A and Advertising expense expectations compared to most recent year.'));
-	$('.depreciation').prepend('<td>' + firstChildGet('.depreciation') + '</td>');
-	$('.advertising').prepend('<td>' + costGrow * firstChildGet('.advertising') + '</td>');
-	$('.research_and_development').prepend('<td>' + costGrow * firstChildGet('.research_and_development') + '</td>');
-	$('.sales_general_and_administrative').prepend('<td>' + costGrow * firstChildGet('.sales_general_and_administrative') + '</td>');
-	newTotalCostandExp += firstChildGet('.depreciation');
-	newTotalCostandExp += firstChildGet('.advertising');
-	newTotalCostandExp += firstChildGet('.research_and_development');
-	newTotalCostandExp += firstChildGet('.sales_general_and_administrative');
-	newTotalCostandExp += newCostandExp;
-	$('.total_costs_and_expenses').prepend('<td>' + newTotalCostandExp + '</td>');
-	$('.income_from_operations').prepend('<td>' + (totalRevenue - newTotalCostandExp) + '</td>');
+/////////////////////////////////////////
+//Remove $ and commas, then parse to Int.
+/////////////////////////////////////////
+var setToNum = function(number){
+	number = parseFloat(number.replace('$','').replace(',',''));
+	return number;
 }
 
-///////////////////////////////////
-// Revenue through Net Income
-///////////////////////////////////
-var proFormaNetIncome = function(){
-	$('.interest_and_other_income').prepend('<td>' + 0 + '</td>');
-	var netIncomeFromOps =  firstChildGet('.income_from_operations') - firstChildGet('.interest_and_other_income');
-	$('.net_income_from_continuing_operations').prepend('<td>' + 0 + '</td>');
-	var taxes = Math.max(netIncomeFromOps * (1 * getRatio('.interest_rate') ),0);
-	var futureDebttoAssetRatio = parseFloat(prompt('The current Debt to Total Assets ratio is '+ $('.Debt_Ratio').children(':nth-child(2)').html() +', what how do you think it will look in the future?'));
-	var interestExpense = (getRatio('.interest_rate') * futureDebttoAssetRatio * firstChildGet('.total_assets')).toFixed(2)
-	$('.interest_expense').prepend('<td>' + interestExpense + '</td>');
-	$('.tax_expense').prepend('<td>' + taxes.toFixed(2) + '</td>');
-	var futureDiscontinuedOperations = parseFloat(prompt('Any assumptions concerning future discontinued operations? (Dollar figure)')).toFixed(2)
-	$('.net_income_from_discontinued_operations').prepend('<td>' + futureDiscontinuedOperations + '</td>');
-	var futureNetIncome =  futureDiscontinuedOperations - interestExpense - futureDebttoAssetRatio + netIncomeFromOps
-	$('.net_income').prepend('<td>' + futureNetIncome + '</td>');
+///////////
+// PROFORMA
+///////////
+
+var proforma = function(){
+	newRevenueYear();
+	appendToForm( _.keys(revObj), revObj, incomeObj['total_revenue']);
+	costAndExpense();
+	appendToForm( ['cost_and_expenses','total_revenue'], incomeObj, incomeObj['total_revenue'])	;
+	newExpenseCost();
+	appendToForm( ['depreciation','advertising','research_and_development','sales_general_and_administrative', 'total_costs_and_expenses'], incomeObj, incomeObj['total_revenue'])	;
+	newIncomeFromOperations();
+	newIntandOtherIncome();
+	newIncomeFromContinuingOperations();
+	newTaxExpense();
+	newInterestExpense();
+	newNetIncome();
+	appendToForm(['net_income','net_income_from_discontinued_operations','income_from_operations', 'interest_and_other_income','net_income_from_continuing_operations','tax_expense','interest_expense'], incomeObj, incomeObj['total_revenue'])
+}
+
+/////////////////////
+// Appender
+/////////////////////
+var appendToForm = function(array, object, total){
+	_.each(array, function(x){
+		$('.'+ x).prepend('<td class="column" data-cs="'+parseFloat(object[x]/ total).toFixed(2) +'%" data-original="$'+parseFloat(object[x]).toFixed(2) +'">$' + parseFloat(object[x]).toFixed(2) + '</td>');
+	})
+}
+
+//////////////////////////////////
+// Begin Proforma Revenue & Cost
+//////////////////////////////////
+
+var newRevenueYear = function(){
+	var newTotalRevenue = 0;
+	_.each(_.keys(revObj),function(x){
+		var grow = parseFloat(prompt('How much do you think ' + x))
+		newSegmentRevenue = grow* (revObj[x][revObj[x].length-1] || revObj[x])
+		revObj[x] = (newSegmentRevenue)
+		newTotalRevenue += newSegmentRevenue
+	})
+	incomeObj['total_revenue'] = newTotalRevenue;
+}
+
+var costAndExpenseArray = [];
+var costAndExpense = function(){
+	var newTotalCost = 0;
+	var array = [];
+	_.each(_.keys(incomeObj) , function(x){
+		if(x.substr(0,25)==='cost_and_expenses_segment'){
+			costAndExpenseArray.push(x);
+			var growth = parseFloat(prompt('What are the future costs expectations for ' + x + '?'));
+			var newCost = growth * (incomeObj[x][0] || incomeObj[x]);
+			incomeObj[x] = newCost;
+			newTotalCost += newCost;
+		} else {console.log('skip')}
+	});
+	incomeObj['cost_and_expenses'] = newTotalCost;
+}
+
+//////////////////////////////////
+// Build Expenses
+//////////////////////////////////
+
+var newExpenseCost = function(){
+	var growth = parseFloat(prompt('What are the future costs expectations for Depreciation, advertising, R&D, and SG&A?'));
+	var newTotalCost = 0;
+	_.each(['depreciation','advertising','research_and_development','sales_general_and_administrative'], function(x){
+			var newCost = growth * parseFloat(incomeObj[x][0] || incomeObj[x]);
+			incomeObj[x] = newCost;
+			newTotalCost += newCost;
+		}
+	);
+	newTotalCost += incomeObj['cost_and_expenses']
+	incomeObj['total_costs_and_expenses'] = newTotalCost;
 }
 
 
+//////////////////////////////////
+// Income From Operations & Other
+//////////////////////////////////
 
-
-var proFormaIncomeStmt = function(){
+var newIncomeFromOperations = function(){
+	incomeObj['income_from_operations'] = incomeObj['total_revenue'] - incomeObj['total_costs_and_expenses']
 }
 
+var newIntandOtherIncome = function(){
+	console.log('hi')
+	var growth = parseFloat(prompt('What are your future expectations for Interest and other Income?'));
+	var newIncome = growth * parseFloat(incomeObj['interest_and_other_income'][0] || incomeObj['interest_and_other_income']);
+	incomeObj['interest_and_other_income'] = newIncome;
+}
 
+var newIncomeFromContinuingOperations = function(){
+	incomeObj['net_income_from_continuing_operations'] = incomeObj['total_revenue'] + incomeObj['interest_and_other_income']
+}
 
+var newTaxExpense = function(){
+	incomeObj['tax_expense'] = incomeObj['net_income_from_continuing_operations'] * assumption.tax_rate;
+}
 
+var newInterestExpense = function(){
+	incomeObj['interest_expense'] = incomeObj['interest_expense'][0] || incomeObj['interest_expense'];
+	alert('Interest Expense is assumped to remain the same.');
+}
 
+var newNetIncome = function(){
+	incomeObj['net_income_from_discontinued_operations'] = parseFloat(prompt('Any assumptions concerning future discontinued operations? (Dollar figure)')).toFixed(2);
+	incomeObj['net_income'] = parseFloat(incomeObj['net_income_from_discontinued_operations']+incomeObj['net_income_from_continuing_operations']-incomeObj['tax_expense']);
+}
