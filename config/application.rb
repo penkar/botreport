fd15ffd150ff8011c7e2c2dfc0674c18029400e2
jsp <jspenkar@gmail.com>
2014-11-10 16:20:37 -0600

    Change made for devise in config application.rb

--- config/application.rb
+++ config/application.rb
@@ -3,6 +3,9 @@ require File.expand_path('../boot', __FILE__)
 require 'csv'
 require 'rails/all'
 
+#Added 11/10/2014 to deal with devise on rails. 
+config.assets.initialize_on_precompile = false
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
