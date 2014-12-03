89dc73f17783fa4bf54b0130da059c953fcddca5
jsp <jspenkar@gmail.com>
2014-12-03 17:26:40 -0600

    American option prices are being added correctly! Huzzah!

--- app/assets/javascripts/american_options.js
+++ app/assets/javascripts/american_options.js
@@ -2,11 +2,14 @@ $(document).ready(function(){
 	$('form').on('ajax:success', function(event, data, status, xhr) {
 		prices.call = data.call;
 		prices.put = data.put;
-
+		americanOption();
 	});
 });
 var prices = {};
 
 var americanOption = function(){
-	
+	$('#cp').contents().remove()
+	$('#cp').append(prices.call)
+	$('#pp').contents().remove()
+	$('#pp').append(prices.put)
 }
