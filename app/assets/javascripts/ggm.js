cfd889fd476884f998c4775db9ad018bae0088b3
jsp <jspenkar@gmail.com>
2014-12-11 23:52:49 -0600

    GGM Javascript working correctly on initial load.

--- app/assets/javascripts/ggm.js
+++ app/assets/javascripts/ggm.js
@@ -1,38 +1,39 @@
-$(document).ready(function(){
+var ready = function(){
 	$('form').on('ajax:success', function(event, data, status, xhr) {
 		count.variable = data;
 		console.log(data);
 		decide();
 		$(count.empty).val(data)
 	});
-});
-var variable = ''
-
-var count = {
-	empty:'',
-	variable:''
-}
-
-var decide = function(){
-	count.count = 0
-	if ($('#growth').val().length !==0){
-		count.count += 1
-	} else {
-		count.empty = '#growth'
+	var variable = ''
+	var count = {
+		empty:'',
+		variable:''
 	}
-	if ($('#value').val().length !==0){
-		count.count += 1
-	} else {
-		count.empty = '#value'
-	}
-	if ($('#rate').val().length !==0){
-		count.count += 1
-	} else {
-		count.empty = '#rate'
-	}
-	if ($('#dividend').val().length !==0){
-		count.count += 1
-	} else {
-		count.empty = '#dividend'
+	var decide = function(){
+		count.count = 0
+		if ($('#growth').val().length !==0){
+			count.count += 1
+		} else {
+			count.empty = '#growth'
+		}
+		if ($('#value').val().length !==0){
+			count.count += 1
+		} else {
+			count.empty = '#value'
+		}
+		if ($('#rate').val().length !==0){
+			count.count += 1
+		} else {
+			count.empty = '#rate'
+		}
+		if ($('#dividend').val().length !==0){
+			count.count += 1
+		} else {
+			count.empty = '#dividend'
+		}
 	}
 }
+
+$(document).ready(ready);
+$(document).on('page:load', ready)
