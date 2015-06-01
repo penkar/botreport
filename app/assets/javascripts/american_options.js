e2d1756c2e779406393d4970e4f4d8b676254fa6
jsp <jspenkar@gmail.com>
2015-05-31 22:42:04 -0500

    Removed ggm, and consolidated code.

--- app/assets/javascripts/american_options.js
+++ app/assets/javascripts/american_options.js
@@ -1,10 +1,28 @@
 var ready = function(){
 	$('form').on('ajax:success', function(event, data, status, xhr) {
-		americanOption(data.call, data.put);
+		if(document.getElementById('cp')){americanOption(data.call, data.put)}
+		if(document.getElementById('growth')){decide(data);}
 	});
 	var americanOption = function(call, put){
 		document.getElementById('cp').innerHTML = call;
 		document.getElementById('pp').innerHTML = put;
+		console.log('decider');
+	};
+	var decide = function(data){
+		var growth = document.getElementById('growth');
+		var value = document.getElementById('value');
+		var rate = document.getElementById('rate');
+		var dividend = document.getElementById('dividend');
+		if (!growth.value){
+			growth.value = data;
+		} else if (!value.value){
+			value.value = data;
+		} else if (!rate.value){
+			rate.value = data;
+		} else if (!dividend.value){
+			dividend.value = data;
+		}
+		console.log('decider');
 	}
 };
 
