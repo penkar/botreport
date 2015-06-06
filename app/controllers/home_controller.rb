f75b68dbf0ee01e5b59b9d0f8c158ac56ee02ad1
jsp <jspenkar@gmail.com>
2015-06-05 22:26:58 -0500

    removed additional jquery requirements.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -40,7 +40,6 @@ class HomeController < ApplicationController
     hash[:value]= params["value"].to_f if params["value"].length>0
     hash[:rate]= params["rate"].to_f if params["rate"].length>0
     variable = FinanceEngine::Equity.gordon_growth_model(hash).round(2)
-    p variable
     render json: variable
   end
 end
